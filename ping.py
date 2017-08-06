279f89f0d01e64ef917e1976b2355d296fe11161
ProgrammingStuff <30649857+ProgrammingStuff@users.noreply.github.com>
2017-08-06 12:42:47 +0300

    deleted https

--- ping.py
+++ ping.py
@@ -5,5 +5,5 @@ def ping(address):
   return not os.system('ping %s -n 1' % (address,))
    
 while True:
-  ping("https://uhc-server.herokuapp.com")
-  time.sleep(1740)
+  ping("uhc-server.herokuapp.com")
+  time.sleep(600)
