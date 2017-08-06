68808cfe059c45cd63b979cac1642e93356abb5c
ProgrammingStuff <30649857+ProgrammingStuff@users.noreply.github.com>
2017-08-06 12:37:55 +0300

    deleted indent

--- ping.py
+++ ping.py
@@ -1,9 +1,9 @@
 import os
 import time
 
-  def ping(address):
-    return not os.system('ping %s -n 1' % (address,))
+def ping(address):
+  return not os.system('ping %s -n 1' % (address,))
    
-   while True:
-    ping("https://uhc-server.herokuapp.com")
-    time.sleep(1740)
+while True:
+  ping("https://uhc-server.herokuapp.com")
+  time.sleep(1740)
